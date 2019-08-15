ecf5df1ba37deb6c8f5d877106803e8893d10c81
Saussereau Dominique <domsau2@yahoo.fr>
2019-08-15 12:26:03 +0200

    Create main.cpp
    NE FONCTIONNE PAS ! 
    DO NOT WORK!

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+// NE FONCTIONNE PAS ! DO NOT WORK!
+//istring
+//itime
+using namespace std;
+
+struct variantePartieBataille // Partie ?
+{
+bool carteCacheeBataille = true;
+bool payePerdPli;
+bool pat;
+bool debutMemeCartes;
+int nombreCartes;
+} MaPartie;
+
+void choixVariantes(){
+cout << "carteCacheeBataille ?" << endl;
+cin >> carteCacheeBataille.MaPartie;
+cout << "payePerdPli  ?" << endl;
+cin >> payePerdPli.MaPartie;
+cout << "carteCacheeBataille ?" << endl;
+cin >> carteCacheeBataille.MaPartie;
+cout << "carteCacheeBataille ?" << endl;
+cin >> carteCacheeBataille.MaPartie;
+cout << "carteCacheeBataille ?" << endl;
+cin >> carteCacheeBataille.MaPartie;
+
+
+
+};
+
+void AfficheVariantes(){
+cout << "carteCacheeBataille : " << carteCacheeBataille << endl;
+cout << "payePerdPli : " << payePerdPli << endl;
+cout << "pat :" << pat << endl;
+cout << "debutMemeCartes :" << debutMemeCartes << endl;
+cout << "nombreCartes :" << nombreCartes << endl;
+};
+
+
+int main()
+{
+//    srand
+struct MaPartie;
+carteCacheeBataille.MaPartie = true;
+payePerdPli.MaPartie = true;
+pat.MaPartie = true;
+debutMemeCartes.MaPartie = true;
+nombreCartes.MaPartie = 54;
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
